e419a744c19bb7aba19df2d0826ef5121cc9045c
Sourabhkhera11 <sourabhkhera11@gmail.com>
2025-12-29 15:37:18 +0530

    VueWithTs

---
+++ P1/src/App.vue
@@ -0,0 +1,66 @@
+<script lang="ts">
+  type fetchDataType=Array<Object>;
+  import { defineComponent } from 'vue';
+  import card from "./components/card.vue";
+  export default defineComponent({
+    components:{
+      card
+    },
+    data: function(){
+      return{
+        fetchData:[] as fetchDataType,
+      };
+    },
+    methods:{
+      async fetchCountries(){
+        try{
+          const response= await fetch("https://countriesnow.space/api/v0.1/countries/flag/unicode");
+          const data=await response.json();
+          this.fetchData=data.data;
+          console.log(data);
+          
+        }
+        catch(error){
+          console.log(error);
+        }
+      }
+    },
+    mounted(){
+      this.fetchCountries();
+    }
+  })
+</script>
+
+<template>
+  <div class="container">
+    <h1>Flags with country name!</h1>
+    <div class="feed">
+      <div class="cards" v-for="(value,index) in fetchData" :key="index">
+        <card :flag="value.name" :cname="value.unicodeFlag" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  *{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
+  .container{
+    margin: 50px;
+  }
+  .feed{
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: row;
+    margin:20px;
+    margin-top:50px;
+  }
+  .cards{
+    width: 25%;
+    border: 1px solid black;
+  }
+</style>
---
+++ P1/src/components/card.vue
@@ -0,0 +1,32 @@
+<script lang="ts">
+    import { defineComponent } from 'vue';
+    export default defineComponent({
+        props:{
+            flag:String,
+            cname:String,
+        }
+    })
+</script>
+<template>
+        <div class="box">
+            <div class="flag">
+                {{ flag }}
+            </div>
+            <div class="country">
+                {{ cname }}
+            </div>
+        </div>
+</template>
+<style scoped>
+    .box{
+        height: 150px;
+        width: 150px;
+        display: flex;
+        gap: 40px;
+        justify-content: center;
+        align-items: center;
+    }
+    .flag,.country{
+        font-size: 30px;
+    }
+</style>
